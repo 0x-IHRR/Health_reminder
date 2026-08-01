15dc8f3b12c9bdb43a0932daa012e25370b7dc52
0x-IHRR <wzx824030716@gmail.com>
2026-08-01 11:23:13 +0800

    Increase large reminder text size

--- Sources/HealthReminder/ReminderOverlayViews.swift
+++ Sources/HealthReminder/ReminderOverlayViews.swift
@@ -529,8 +529,8 @@ struct OverlayVisualStyle {
             titleFontSize = 20
             bodyFontSize = 14
         case "large":
-            titleFontSize = 26
-            bodyFontSize = 17
+            titleFontSize = 32
+            bodyFontSize = 21
         default:
             titleFontSize = 23
             bodyFontSize = 15.5
